2502ae17cf88303ce51e2000e5359efd6d569eb7
kkkkrk <62200028+kkkkrk@users.noreply.github.com>
2021-07-17 17:55:53 +0900

    Update 03. key_event.py

--- 도전과제/03. key_event.py
+++ 도전과제/03. key_event.py
@@ -6,22 +6,22 @@ from  PyQt5.QtWidgets import QApplication, QWidget, QLabel
 class MyApp(QWidget):
     def __init__(self):
         super().__init__()
-        self.setFixedSize(1024,768)
+        self.setFixedSize(400,400)
         self.setWindowTitle('GA Mario')
+        self.label_text = QLabel(self)
         self.show()
-
-        self.label_text = QLabel
 
     #키를 누를 때
     def keyPressEvent(self, event):
         key = event.key()
         self.label_text.setText(str(key) + ' press')
-        self.label_text.setGeometry(200, 150, 50, 100)
+        self.label_text.setGeometry(0, 150, 50, 100)
 
     # 키를 뗄 때
     def keyReleaseEvent(self, event):
         key = event.key()
-        print(str(key) + ' release')
+        self.label_text.setText(str(key) + ' release')
+        self.label_text.setGeometry(0, 150, 50, 100)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
